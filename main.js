b6a7ead8b07f0fff8c0e691004708675a19d5ed1
Debikapaul <82105390+Debikapaul@users.noreply.github.com>
2021-09-28 22:26:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas=createCanvas(200,200);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(200,200);
+    video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+
+function draw(){
+    image(video,0,0,200,200);
+}
+
+function take_snapshot(){
+    save('My Lipstick Filter Image.png');
+}
+
+function modelLoaded(){
+    console.log("Posenet is Initialised");
+}
+
+function gotPoses(){
+    if(results.length > 0 ){
+        console.log(results);
+        console.log("lip x="+ results[0].pose.lip.x);
+        console.log("lip y="+ results[0].pose.lip.y);
+    }
+}
